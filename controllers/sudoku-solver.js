6309d2636d7d593579cd5b13b4f260b9e8fecff7
Meheer007 <>
2021-11-21 19:56:30 +0000

    1

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,4 +1,4 @@
-class SudokuSolver {
+ class SudokuSolver {
 
     validate(puzzleString) { }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,82 @@ const assert = chai.assert;
 const Solver = require('../controllers/sudoku-solver.js');
 let solver = new Solver;
 
+
+let vp = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+
 suite('UnitTests', () => {
+
+    suite("solver tests",() => {
+
+        test("Logic handles a valid puzzle string of 81 characters", (done) => {
+            let complete = "135762984946381257728459613694517832812936745357824196473298561581673429269145378";
+            assert.equal(solver.solve(vp), complete);
+            done();
+        });
+
+        test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", (done) => {
+            let inva = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.g7.";
+            assert.equal(solver.solve(inva), false);
+            done();
+        });
+
+        test("Logic handles a puzzle string that is not 81 characters in length", (done) => {
+            let inva = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16";
+            assert.equal(solver.solve(inva), false);
+            done();
+        });
+
+        test("Logic handles a valid row placement", (done) => {
+            assert.equal(solver.checkRowPlacement(vp, "A", "2", "9"), true);
+            done();
+        });
+
+        test("Logic handles an invalid row placement", (done) => {
+            assert.equal(solver.checkRowPlacement(vp, "A", "2", "9"), true);
+            done();
+        });
+
+        test("Logic handles a valid column placement", (done) => {
+            assert.equal(solver.checkColPlacement(vp, "A", "2", "8"), true);
+            done();
+        });
+
+        test("Logic handles an invalid column placement", (done) => {
+            assert.equal(solver.checkColPlacement(vp, "A", "2", "9"), false);
+            done();
+        });
+
+        test("Logic handles a valid region (3x3 grid) placement", (done) => {
+            assert.equal(solver.checkRegionPlacement(vp, "A", "2", "3"), true);
+            done();
+        });
+
+        test("Logic handles an invalid region (3x3 grid) placement", (done) => {
+            assert.equal(solver.checkRegionPlacement(vp, "A", "2", "1"), false);
+            done();
+        });
+
+        test("Valid puzzle strings pass the solver", (done) => {
+            assert.equal(solver.solve(vp), "135762984946381257728459613694517832812936745357824196473298561581673429269145378");
+            done();
+        });
+
+        test("Invalid puzzle strings fail the solver", (done) => {
+            let inva = "115..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+            assert.equal(solver.solve(inva), false);
+            done();
+        });
+
+        test("Solver returns the the expected solution for an incomplete puzzzle", function (done) {
+            assert.equal(
+            solver.solve(
+                "..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1"
+            ),
+                "218396745753284196496157832531672984649831257827549613962415378185763429374928561"
+            );
+      done();
+    });
+
+    });
 
 });
